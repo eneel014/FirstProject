97cff922e5dfb2972ef60b8982e01e8c3cbb2e8d
KMCINT\enel <eneldc@2binteractive.com.au>
2018-05-28 06:41:42 +0800

    Add test comment

--- edit-client.php
+++ edit-client.php
@@ -9,6 +9,8 @@ $query = "SELECT * FROM tbl_clients	 WHERE id = '$client_id'";
 $result = $conn->query($query); 
 $r = $result->fetch_assoc(); ?>
 
+<!-- Test -->
+
 <form method="POST" name="editClientForm" id="editClientForm">
 	<div class="section">
 		<div class="row"><div class="col s12">&nbsp;</div></div>
